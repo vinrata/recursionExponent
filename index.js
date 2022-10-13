cfa97fc19d28bd5664e3b62d9c4ae9440d031f5e
vinrata <57158224+vinrata@users.noreply.github.com>
2022-10-13 13:31:42 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function power(number, exponent) {
 
 
-if (exponent ===1)return  number;
-return number *power(number, exponent - 1)  ;
+if (exponent === 1)return number;
+return number * power(number, exponent - 1);
 }
 console.log(power(2,5));
